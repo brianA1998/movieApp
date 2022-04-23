ae63f8dd909b431497055e03513d39ed78c400b7
brianA1998 <brian.alaniz@hotmail.com>
2022-04-22 23:28:28 -0300

    Add viewBinding in project

--- app/src/main/java/com/example/movieapp/MovieDetailFragment.kt
+++ app/src/main/java/com/example/movieapp/MovieDetailFragment.kt
@@ -1,14 +1,16 @@
 package com.example.movieapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.movieapp.databinding.FragmentMovieDetailBinding
 
 
 class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
+    private lateinit var binding: FragmentMovieDetailBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieDetailBinding.bind(view)
     }
 }
