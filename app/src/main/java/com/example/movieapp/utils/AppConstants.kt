c0b870e17e40c8af55629158fe096953890e5e63
brianA1998 <brian.alaniz@hotmail.com>
2022-04-28 22:03:16 -0300

    WebService and Retrofit for get information with api

---
+++ app/src/main/java/com/example/movieapp/utils/AppConstants.kt
@@ -0,0 +1,8 @@
+package com.example.movieapp.utils
+
+object AppConstants {
+
+    //API
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "379fd1bc95a20ff8d54762a98e93795e"
+}
