b41eb99ecaf34307ba63a857d1e02a65f84441fe
brianA1998 <brian.alaniz@hotmail.com>
2022-05-03 23:43:17 -0300

    Called methods of webService from data source

--- app/src/main/java/com/example/movieapp/data/remote/MovieDataSource.kt
+++ app/src/main/java/com/example/movieapp/data/remote/MovieDataSource.kt
@@ -1,19 +1,21 @@
 package com.example.movieapp.data.remote
 
 import com.example.movieapp.data.model.MovieList
+import com.example.movieapp.repository.WebService
+import com.example.movieapp.utils.AppConstants
 
-class MovieDataSource {
+class MovieDataSource(private val webService: WebService) {
 
-    fun getUpcomingMovies(): MovieList {
-        return MovieList()
+    suspend fun getUpcomingMovies(): MovieList {
+        return webService.getUpcomingMovies(AppConstants.API_KEY)
     }
 
-    fun getTopRatedMovies(): MovieList {
-        return MovieList()
+    suspend fun getTopRatedMovies(): MovieList {
+        return webService.getTopRatedMovies(AppConstants.API_KEY)
     }
 
-    fun getPopularMovies(): MovieList {
-        return MovieList()
+    suspend fun getPopularMovies(): MovieList {
+        return webService.getPopularMovies(AppConstants.API_KEY)
     }
 
 }
